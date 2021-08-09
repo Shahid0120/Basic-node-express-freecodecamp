3b96b6fa0ba1b04124a79f299d190e1c514e4677
Shahid0120 <shahid.hussain0120@gmail.com>
2021-08-09 14:52:54 +1000

    basic node 5/12

--- myApp.js
+++ myApp.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var app = express();
+var absolutePathIndex = __dirname + "/views/index.html";
+var absolutePathPublic = __dirname + "/public/style.css";
+
+app.get("/", function(req, res) {
+    res.sendFile(absolutePathIndex);
+});
+
+app.use("/public/style.css", express.static(absolutePathPublic));
+
+app.get ("/json", function(req, res) {
+    res.json({
+    "message": "Hello json"
+    });
+});
+
+console.log('Hello World');
+
 
 
 
